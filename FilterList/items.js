0214ff127303632bef4adb9803ffbfb045e13b70
Sripathi Avinash Kumar <65992275+SreepathiAvinashKumar@users.noreply.github.com>
2023-05-07 12:35:29 +0530

    changed the images

--- FilterList/items.js
+++ FilterList/items.js
@@ -12,7 +12,7 @@ const products = [
       title: 'albany table',
       company: 'marcos',
       image:
-        'https://media.istockphoto.com/id/1392454801/photo/lynn-canyon-park.jpg?s=1024x1024&w=is&k=20&c=S3ypvgjj5rh_lpXICX4BJip6mPVyY0jbQwqiBo0eK_k=',
+        'https://images.freeimages.com/images/large-previews/fda/newport-bridge-sunset-1634624.jpg',
       price: 79.99,
     },
     {
@@ -20,7 +20,7 @@ const products = [
       title: 'accent chair',
       company: 'caressa',
       image:
-        'https://dl.airtable.com/.attachments/f292eaec4fea8a791b037c759ae559c9/2d5e34d5/product-4.jpeg',
+        'https://images.freeimages.com/images/large-previews/fda/newport-bridge-sunset-1634624.jpg',
       price: 25.99,
     },
     {
@@ -28,7 +28,7 @@ const products = [
       title: 'wooden table',
       company: 'caressa',
       image:
-        'https://dl.airtable.com/.attachments/5b3ad76dfd6ca5e31810cb99141c7ede/69829b2f/pexels-dominika-roseclay-1139785.jpg',
+        'https://images.freeimages.com/images/large-previews/fda/newport-bridge-sunset-1634624.jpg',
       price: 45.99,
     },
     {
@@ -36,7 +36,7 @@ const products = [
       title: 'dining table',
       company: 'caressa',
       image:
-        'https://dl.airtable.com/.attachments/06bbede65bd09196b4a81a8e7b59e683/8af0aeb5/product-8.jpg',
+        'https://images.freeimages.com/images/large-previews/fda/newport-bridge-sunset-1634624.jpg',
       price: 6.99,
     },
     {
@@ -44,7 +44,7 @@ const products = [
       title: 'sofa set',
       company: 'liddy',
       image:
-        'https://dl.airtable.com/.attachments/443aa7884207dae7c3cc127262a2f993/d4f33110/product-1.jpeg',
+        'https://images.freeimages.com/images/large-previews/fda/newport-bridge-sunset-1634624.jpg',
       price: 69.99,
     },
     {
@@ -52,7 +52,7 @@ const products = [
       title: 'modern bookshelf',
       company: 'marcos',
       image:
-        'https://dl.airtable.com/.attachments/a889b1928b134c2ca0b5bbca32ea9abf/65c8a9a6/product-7.jpg',
+        'https://images.freeimages.com/images/large-previews/fda/newport-bridge-sunset-1634624.jpg',
       price: 8.99,
     },
     {
@@ -60,7 +60,7 @@ const products = [
       title: 'emperor bed',
       company: 'liddy',
       image:
-        'https://dl.airtable.com/.attachments/10fdf29ae17f2d1f98770ae42584d021/82b9403f/product-6.jpg',
+        'https://images.freeimages.com/images/large-previews/fda/newport-bridge-sunset-1634624.jpg',
       price: 21.99,
     },
     {
@@ -68,7 +68,7 @@ const products = [
       title: 'utopia sofa',
       company: 'marcos',
       image:
-        'https://dl.airtable.com/.attachments/6ac7f7b55d505057317534722e5a9f03/9183491e/product-3.jpg',
+        'https://images.freeimages.com/images/large-previews/fda/newport-bridge-sunset-1634624.jpg',
       price: 39.95,
     },
     {
@@ -76,7 +76,7 @@ const products = [
       title: 'entertainment center',
       company: 'liddy',
       image:
-        'https://dl.airtable.com/.attachments/da5e17fd71f50578d525dd5f596e407e/d5e88ac8/product-2.jpg',
+        'https://images.freeimages.com/images/large-previews/fda/newport-bridge-sunset-1634624.jpg',
       price: 29.98,
     },
     {
@@ -84,7 +84,7 @@ const products = [
       title: 'albany sectional',
       company: 'ikea',
       image:
-        'https://dl.airtable.com/.attachments/05ecddf7ac8d581ecc3f7922415e7907/a4242abc/product-1.jpeg',
+        'https://images.freeimages.com/images/large-previews/fda/newport-bridge-sunset-1634624.jpg',
       price: 10.99,
     },
     {
@@ -92,7 +92,7 @@ const products = [
       title: 'leather sofa',
       company: 'liddy',
       image:
-        'https://dl.airtable.com/.attachments/3245c726ee77d73702ba8c3310639727/f000842b/product-5.jpg',
+        'https://images.freeimages.com/images/large-previews/fda/newport-bridge-sunset-1634624.jpg',
       price: 9.99,
     },
   ];
